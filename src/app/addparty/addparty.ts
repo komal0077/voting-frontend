6334c0be08264903cf8a11595140babf862370ba
komal <komalme7@gmail.com>
2026-03-10 14:21:36 +0530

    Fixed Buttons

--- src/app/addparty/addparty.ts
+++ src/app/addparty/addparty.ts
@@ -1,5 +1,3 @@
-
-
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Vote } from '../services/vote';
@@ -13,6 +11,7 @@ import { Router } from '@angular/router';
   templateUrl: './addparty.html'
 })
 export class AddParty {
+
   party = {
     name: '',
     leader: '',
@@ -22,30 +21,60 @@ export class AddParty {
   constructor(private voteService: Vote, private router: Router) {}
 
   submit() {
+
     this.voteService.addParty(this.party).subscribe({
+
       next: () => {
+
         Swal.fire({
           title: 'Party Added!',
           text: 'What to proceed further?',
           icon: 'success',
+
           showCancelButton: true,
+
           confirmButtonText: 'Add Candidate',
-          cancelButtonText: 'Add Another Party'
-        }).then((res) => {
+          cancelButtonText: 'Add Another Party',
+
+          confirmButtonColor: '#f97316',  // orange button
+          cancelButtonColor: '#374151',   // gray button
+
+          background: '#1f2937',          // dark popup background
+          color: '#ffffff'                // white text
+        })
+
+        .then((res) => {
+
           if (res.isConfirmed) {
             this.router.navigate(['/add-candidate']);
-          } else {
-            this.party = { name: '', leader: '', symbol: '' };
           }
+          else {
+            this.party = {
+              name: '',
+              leader: '',
+              symbol: ''
+            };
+          }
+
         });
+
       },
+
       error: () => {
+
         Swal.fire({
           icon: 'error',
           title: 'Error',
-          text: 'Failed to add party.'
+          text: 'Failed to add party.',
+          confirmButtonColor: '#ef4444',
+          background: '#1f2937',
+          color: '#ffffff'
         });
+
       }
+
     });
+
   }
+
 }
