5301f739dcb40a740bc1578ccf70954c4fbb74c3
komal <komalme7@gmail.com>
2026-03-10 14:09:43 +0530

    checking

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,5 +1,4 @@
 
-
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { Auth } from '../services/auth';
@@ -13,33 +12,13 @@ import { CommonModule } from '@angular/common';
 })
 export class Navbar {
 
-  isDark = false;
-
-  constructor(public auth: Auth, private router: Router) {
-    // Load saved theme
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark') {
-      this.isDark = true;
-      document.body.classList.add('dark-theme');
-    }
-  }
   isMenuOpen = false;
 
-  toggleTheme() {
-    this.isDark = !this.isDark;
-
-    if (this.isDark) {
-      document.body.classList.add('dark-theme');
-      localStorage.setItem('theme', 'dark');
-    } else {
-      document.body.classList.remove('dark-theme');
-      localStorage.setItem('theme', 'light');
-    }
-  }
+  constructor(public auth: Auth, private router: Router) {}
 
   logout() {
     this.auth.logout();
     this.router.navigate(['/login']);
   }
-}
 
+}
